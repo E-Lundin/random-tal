11cda8a45e20f54f1c2080055bfac8d2f8439a53
Erixen <eric.lundin@hotmail.se>
2020-11-16 10:30:45 +0100

    Add project files.

---
+++ random-tal/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace random_tal
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guessButton = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // guessButton
+            // 
+            this.guessButton.Location = new System.Drawing.Point(261, 169);
+            this.guessButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(71, 23);
+            this.guessButton.TabIndex = 0;
+            this.guessButton.Text = "Gissa";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(75, 170);
+            this.inputBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(174, 23);
+            this.inputBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(14, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 65);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gissa nummret!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(75, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Gissa ett nummer mellan 0 och 100";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(75, 233);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(257, 69);
+            this.outputBox.TabIndex = 4;
+            this.outputBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 336);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.guessButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox outputBox;
+    }
+}
+
