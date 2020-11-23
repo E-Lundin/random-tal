206737777553a568e641794c82bf42bebc9079b0
Eric <62556676+E-Lundin@users.noreply.github.com>
2020-11-23 08:20:52 +0100

    Korrigera indentering

--- gissa_tal.cs
+++ gissa_tal.cs
@@ -5,24 +5,29 @@ namespace GissaTal
     class Program
     {
 
-        static int randomNumber(int min, int max){
+        static int randomNumber(int min, int max)
+        {
             Random random = new Random();
             return random.Next(min, max);
         }
 
-        static int getInput(){
-            while(true){
+        static int getInput()
+        {
+            while (true)
+            {
                 Console.WriteLine("Gissa ett tal mellan 0 och 100: ");
                 string input = Console.ReadLine();
                 int guess;
 
                 bool validNumber = Int32.TryParse(input, out guess);
 
-                if (!validNumber){
+                if (!validNumber)
+                {
                     Console.WriteLine("Ogiltig gissning, försök igen!");
-					continue;
+                    continue;
                 }
-                else if (validNumber){
+                else if (validNumber)
+                {
                     return guess;
                 }
             }
@@ -34,18 +39,22 @@ namespace GissaTal
             int guess = -1;
             int number = randomNumber(0, 100);
 
-            while(guess != number){
+            while (guess != number)
+            {
                 guess = getInput();
                 attempts++;
 
-                if (guess == number){
+                if (guess == number)
+                {
                     Console.WriteLine(guess + " är Rätt svar!");
                     Console.WriteLine("Det tog dig " + attempts + " gissningar.");
                 }
-                else if (guess > number){
+                else if (guess > number)
+                {
                     Console.WriteLine(guess + " är för stort!");
                 }
-                else if (guess < number){
+                else if (guess < number)
+                {
                     Console.WriteLine(guess + " är för litet!");
                 }
             }
